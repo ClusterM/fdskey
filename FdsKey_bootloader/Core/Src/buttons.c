53bfdfa06bea828cfc5d45047fb66d71fc314c01
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2023-03-31 14:38:48 +0400

    Bootloader tuning

--- FdsKey_bootloader/Core/Src/buttons.c
+++ FdsKey_bootloader/Core/Src/buttons.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include "buttons.h"
-#include "oled.h"
+#include "settings.h"
 
 static uint8_t up_pressed = 0;
 static uint8_t down_pressed = 0;
@@ -13,25 +13,25 @@ static uint32_t right_hold_time = 0;
 
 uint8_t button_up_holding()
 {
-  uint8_t v = !HAL_GPIO_ReadPin(BUTTON_UP_GPIO_Port, BUTTON_UP_Pin);
+  uint8_t v = !HAL_GPIO_ReadPin(!fdskey_settings.lefty_mode ? BUTTON_UP_GPIO_Port : BUTTON_DOWN_GPIO_Port, !fdskey_settings.lefty_mode ? BUTTON_UP_Pin : BUTTON_DOWN_Pin);
   return v;
 }
 
 uint8_t button_down_holding()
 {
-  uint8_t v = !HAL_GPIO_ReadPin(BUTTON_DOWN_GPIO_Port, BUTTON_DOWN_Pin);
+  uint8_t v = !HAL_GPIO_ReadPin(!fdskey_settings.lefty_mode ? BUTTON_DOWN_GPIO_Port : BUTTON_UP_GPIO_Port, !fdskey_settings.lefty_mode ? BUTTON_DOWN_Pin : BUTTON_UP_Pin);
   return v;
 }
 
 uint8_t button_left_holding()
 {
-  uint8_t v = !HAL_GPIO_ReadPin(BUTTON_LEFT_GPIO_Port, BUTTON_LEFT_Pin);
+  uint8_t v = !HAL_GPIO_ReadPin(!fdskey_settings.lefty_mode ? BUTTON_LEFT_GPIO_Port : BUTTON_RIGHT_GPIO_Port, !fdskey_settings.lefty_mode ? BUTTON_LEFT_Pin : BUTTON_RIGHT_Pin);
   return v;
 }
 
 uint8_t button_right_holding()
 {
-  uint8_t v = !HAL_GPIO_ReadPin(BUTTON_RIGHT_GPIO_Port, BUTTON_RIGHT_Pin);
+  uint8_t v = !HAL_GPIO_ReadPin(!fdskey_settings.lefty_mode ? BUTTON_RIGHT_GPIO_Port : BUTTON_LEFT_GPIO_Port, !fdskey_settings.lefty_mode ? BUTTON_RIGHT_Pin : BUTTON_LEFT_Pin);
   return v;
 }
 
