04b9fafd7d7d571df5338ae3b36b8688f4cc3996
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2023-03-31 16:16:26 +0400

    Text scroll speed

--- FdsKey/Core/Inc/sideselect.h
+++ FdsKey/Core/Inc/sideselect.h
@@ -7,7 +7,7 @@
 #define SIDE_SELECT_GAME_NAME_FONT FONT_SLIMFONT_8
 #define SIDE_SELECT_SIDE_NAME_FONT FONT_VERDANA_14_BOLD
 #define SIDE_SELECT_HORIZONTAL_SCROLL_SPEED 2
-#define SIDE_SELECT_HORIZONTAL_SCROLL_PAUSE 12
+#define SIDE_SELECT_HORIZONTAL_SCROLL_PAUSE 24
 
 void fds_side_select(char *directory, FILINFO *fno);
 DotMatrixImage* side_select_get_disk_image(uint8_t side, uint8_t side_count);
