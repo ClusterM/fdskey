8d24a70d03ec04bbd70e205301f7c6ca1676fc81
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2023-07-31 09:55:38 +0400

    Version bump to 1.1b

--- FdsKey/Core/Inc/main.h
+++ FdsKey/Core/Inc/main.h
@@ -47,7 +47,7 @@ extern "C" {
 /* USER CODE BEGIN EM */
 #define FDSKEY_VERION_MAJOR 1
 #define FDSKEY_VERION_MINOR 1
-#define FDSKEY_VERION_SUFFIX 0
+#define FDSKEY_VERION_SUFFIX 'b'
 /* USER CODE END EM */
 
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
